32deb64248422729ba1c3f4bfc637580be99c2f2
Arya Zeynep Mete <aryazeynep.mete@gmail.com>
2025-12-11 09:02:19 +0300

    Update pdf parser tests and patches

--- bioagents/graph.py
+++ bioagents/graph.py
@@ -20,6 +20,10 @@ from bioagents.tools.analysis_tools import (
 from bioagents.tools.proteomics_tools import fetch_uniprot_fasta
 from bioagents.tools.tool_universe import tool_universe_call_tool, tool_universe_find_tools
 
+from bioagents.tools.pdf_tools import (
+    fetch_webpage_as_pdf_text,
+    extract_pdf_text_spacy_layout,
+)
 
 class AgentState(dict):
     """
@@ -109,6 +113,8 @@ def create_graph():
         fetch_uniprot_fasta,
         tool_universe_find_tools,
         tool_universe_call_tool,
+        fetch_webpage_as_pdf_text,           # <- NEW
+        extract_pdf_text_spacy_layout,       # <- NEW
     ]
     analysis_tools = [
         calculate_molecular_weight,
